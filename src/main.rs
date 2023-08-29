251fc1c18654565502042e9e26389c34dd928794
Timothy Hjort <timothy.hjort@outlook.com>
2023-08-29 13:36:43 +0200

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::File};
+use std::{collections::HashMap, env, fs::File};
 
 use ::entities::{write_csv_file, ResultEntry};
 use clap::Parser;
@@ -17,28 +17,18 @@ fn allocate_results<T: AsRef<str> + Into<String>>(
     names: &[T],
     content: Vec<NormalizedData>,
 ) -> Vec<ResultEntry> {
-    let chunksize = content.len() / names.len() + 1;
-    debug!("chunksize is {}", chunksize);
-
-    let mut entries = vec![];
-    for (contents, person) in content.chunks(chunksize).zip(names.iter()) {
-        info!(
-            "allocating {} entries to {}",
-            contents.len(),
-            person.as_ref().to_string()
-        );
-
-        for content in contents {
-            entries.push(ResultEntry {
-                user: person.as_ref().to_string(),
-                abstract_: content.abstract_.to_string(),
-                title: content.title.to_string(),
-                authors: content.authors.to_string(),
-                url: content.url.to_string(),
-                doi: content.doi.to_string(),
-            });
-        }
-    }
+    let entries: Vec<ResultEntry> = content
+        .iter()
+        .zip(names.iter().cycle())
+        .map(|(content, person)| ResultEntry {
+            user: person.as_ref().to_string(),
+            abstract_: content.abstract_.to_string(),
+            title: content.title.to_string(),
+            authors: content.authors.to_string(),
+            url: content.url.to_string(),
+            doi: content.doi.to_string(),
+        })
+        .collect();
     debug!("Got {} entries total", entries.len());
 
     entries
@@ -51,12 +41,19 @@ struct Arguments {
     pub output_file: String,
     pub users: Vec<String>,
     #[clap(long)]
+    // Path to a CSV file of data dumped from IEEE Xplore
     pub ieee_source: Option<String>,
     #[clap(long)]
+    /// Path to a CSV file of data dumped from Springer
     pub springer_source: Option<String>,
 
     #[clap(long)]
+    /// Path to a CSV file of data dumped from Scopus
     pub scopus_source: Option<String>,
+
+    #[clap(short, long)]
+    /// Remove duplicate articles based on DOI
+    pub deduplicate: bool,
 }
 fn main() {
     if env::var("RUST_LOG").is_err() {
@@ -80,23 +77,47 @@ fn main() {
         reader.collect::<Vec<NormalizedData>>()
     };
 
-    let mut papers = vec![];
     let handlers: &[(Option<String>, Handler)] = &[
         (args.ieee_source, from_ieee_source),
         (args.springer_source, from_springer_source),
         (args.scopus_source, from_scopus_source),
     ];
-
-    for (source, handler) in handlers {
-        if let Some(filename) = source {
-            info!("Reading content from IEEE source {}", filename);
+    
+    let mut papers: Vec<NormalizedData> = handlers
+        .into_iter()
+        .filter(|(filename, _)| filename.is_some())
+        .map(|(x, handler)| {
+            let filename = x.as_ref().unwrap();
+            info!("Reading content from source {}", filename);
             let file = File::open(filename).expect("Unable to open file");
-            let mut content = handler(file);
+            let content = handler(file);
             debug!("Read {} entries", content.len());
-            papers.append(&mut content);
-        }
-    }
+            content.into_iter()
+        }).flatten().collect()
+        ;
+
     debug!("has total of {} papers", papers.len());
+
+    if args.deduplicate {
+        // Remove duplicates
+        info!("About to filter duplicates");
+        let old_len = papers.len();
+
+        let mut seen: HashMap<String, ()> = HashMap::new();
+        papers = papers.into_iter().filter(|p| {
+            if seen.contains_key(&p.doi) {
+                false
+            } else {
+                seen.insert(p.doi.to_string(), ());
+                true
+            }
+        }).collect();
+        info!(
+            "Filtered {} entries, we now have {} left",
+            old_len - papers.len(),
+            papers.len()
+        );
+    }
 
     let contents = allocate_results(&args.users, papers);
 
